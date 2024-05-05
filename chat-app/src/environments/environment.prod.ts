6df0a91866c78cb20878cb136e9844b5b19a6eea
Gabko14 <gabkolistiak@gmail.com>
2024-05-05 20:49:36 +0200

    Frontend: Backend url to https

--- chat-app/src/environments/environment.prod.ts
+++ chat-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://chatapp-loadbalancer-1430994462.us-east-1.elb.amazonaws.com:5232/' // URL to your production backend
+  apiUrl: 'https://www.nelp.club:5232/' // URL to your production backend
 };
