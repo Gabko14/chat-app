aef86cb6b6e9d08d78bf6cd9e2b47fad5dcba4e4
Gabko14 <gabkolistiak@gmail.com>
2024-05-28 21:19:31 +0200

    Frontend: init prettier, eslint & editorconfig closes #21

--- chat-app/src/app/features/auth/auth.service.ts
+++ chat-app/src/app/features/auth/auth.service.ts
@@ -1,33 +1,35 @@
-import {Injectable, OnDestroy} from '@angular/core';
-import {DialogSetUsernameComponent} from "./dialog-set-username.component";
-import {ReplaySubject, takeUntil} from "rxjs";
-import {MatDialog} from "@angular/material/dialog";
+import { Injectable, OnDestroy } from "@angular/core"
+import { DialogSetUsernameComponent } from "./dialog-set-username.component"
+import { ReplaySubject, takeUntil } from "rxjs"
+import { MatDialog } from "@angular/material/dialog"
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root",
 })
 export class AuthService implements OnDestroy {
+	private readonly destroyed = new ReplaySubject<void>()
 
-  private readonly destroyed = new ReplaySubject<void>();
+	constructor(private dialog: MatDialog) {}
 
-  constructor(private dialog: MatDialog) { }
+	public login() {
+		if (localStorage.getItem("userName") === null) {
+			const dialogRef = this.dialog.open(DialogSetUsernameComponent, {
+				data: { newUsername: "" },
+			})
 
-  public login() {
-    if (localStorage.getItem('userName') === null) {
-      const dialogRef = this.dialog.open(DialogSetUsernameComponent, {
-        data: {newUsername: ""},
-      });
+			dialogRef
+				.afterClosed()
+				.pipe(takeUntil(this.destroyed))
+				.subscribe((newMessage) => {
+					if (newMessage) {
+						localStorage.setItem("userName", newMessage)
+					}
+				})
+		}
+	}
 
-      dialogRef.afterClosed().pipe(takeUntil(this.destroyed)).subscribe((newMessage) => {
-        if (newMessage) {
-          localStorage.setItem('userName', newMessage);
-        }
-      });
-    }
-  }
-
-  ngOnDestroy() {
-    this.destroyed.next();
-    this.destroyed.complete();
-  }
+	ngOnDestroy() {
+		this.destroyed.next()
+		this.destroyed.complete()
+	}
 }
