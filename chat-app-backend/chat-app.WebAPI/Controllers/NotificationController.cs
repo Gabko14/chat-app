4bc5a8a2a3b024d646734ad600df90acb37f8251
Gabko14 <gabkolistiak@gmail.com>
2024-06-02 21:48:15 +0200

    FCM seperating prod/development notifications closes #23
    Also, the sending of the notifications is not handled in the client
    anymore

--- chat-app-backend/chat-app.WebAPI/Controllers/NotificationController.cs
+++ chat-app-backend/chat-app.WebAPI/Controllers/NotificationController.cs
@@ -8,13 +8,6 @@ namespace chat_app.WebApi.Controllers;
 [Route("[controller]")]
 public class NotificationsController(INotificationService notificationService) : ControllerBase
 {
-    
-    [HttpPost("send-everyone-notification")]
-    public async Task<IActionResult> SendNotificationToEveryone([FromBody] Notification notification)
-    {
-        await notificationService.SendNotificationToEveryone(notification);
-        return Ok(new { Message = "Notifications got sent successfully." });
-    }
 
     [HttpPost("add-subscriber")]
     public async Task<IActionResult> AddPushSubscriber(string fcmRegistrationToken)
--- chat-app-backend/chat-app.WebAPI/Hubs/ChatHub.cs
+++ chat-app-backend/chat-app.WebAPI/Hubs/ChatHub.cs
@@ -1,23 +1,32 @@
 ﻿using chat_app.Application.DTOs;
 using chat_app.Application.Interfaces;
+using FirebaseAdmin.Messaging;
 using Microsoft.AspNetCore.SignalR;
 
 namespace chat_app.WebApi.Hubs;
 
-public class ChatHub(IMessageService messageService) : Hub
+public class ChatHub(IMessageService messageService, INotificationService notificationService) : Hub
 {
-    private readonly IMessageService _messageService = messageService;
-
     public async Task SendMessage(string sender, string messageContent)
     {
-        MessageCreateDto message = new MessageCreateDto
+        try
         {
-            Content = messageContent,
-            Sender = sender,
-        };
+            MessageCreateDto message = new MessageCreateDto
+            {
+                Content = messageContent ?? throw new ArgumentNullException(nameof(messageContent)),
+                Sender = sender ?? throw new ArgumentNullException(nameof(sender))
+            };
 
-        MessageReadDto savedMessage = await _messageService.CreateMessageAsync(message);
+            MessageReadDto savedMessage = await messageService.CreateMessageAsync(message);
 
-        await Clients.All.SendAsync("ReceiveMessage", savedMessage.Sender, savedMessage.Content);
+            await notificationService.SendNotificationToEveryone(new Notification
+                { Title = savedMessage.Sender, Body = savedMessage.Content });
+
+            await Clients.All.SendAsync("ReceiveMessage", savedMessage.Sender, savedMessage.Content);
+        }
+        catch (Exception)
+        {
+            throw new HubException("An error occured when sending the message");
+        }
     }
 }
